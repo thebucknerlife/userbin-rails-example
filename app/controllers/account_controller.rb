e90960a455c045928d14c07d29d3eff6601bd7b9
Johan Brissmyr <brissmyr@gmail.com>
2014-01-09 14:14:05 +0100

    Use new before_filter syntax

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,2 +1,3 @@
 class AccountController < ApplicationController
+  before_filter :authenticate_user!
 end
--- config/initializers/userbin.rb
+++ config/initializers/userbin.rb
@@ -5,19 +5,16 @@ Userbin.configure do |config|
   # config.app_id = "YOUR_APP_ID"
   # config.api_secret = "YOUR_API_SECRET"
 
-  config.root_path = "/"
-  config.protected_path = "/account"
-
   # Save a local user when a user logs in with Userbin
-  config.create_user = Proc.new { |profile|
+  config.create_user = -> (profile) do
     User.create! do |user|
       user.userbin_id = profile.id
       user.email      = profile.email
       user.image      = profile.image
     end
-  }
+  end
 
-  config.find_user = Proc.new { |userbin_id|
+  config.find_user = -> (userbin_id) do
     User.find_by_userbin_id(userbin_id)
-  }
+  end
 end
